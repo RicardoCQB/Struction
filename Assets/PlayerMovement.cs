107c7380d3cbabd5ff5215ffc425c2d1466910e4
Ricardo Brioso <ricardo_cqb@hotmail.com>
2020-11-01 15:22:08 +0000

    Added movement script

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    [Space]
+    [Header("Player Stats")]
+    public float speed = 10;
+    public float playerJumpForce = 50;
+
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Gets the components of the player Game Object        
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector2 dir = new Vector2(x, y);
+
+        Walk(dir);
+
+
+    }
+
+    private void Walk(Vector2 dir)
+    {
+        rb.velocity = new Vector2(dir.x * speed, rb.velocity.y);
+    }
+
+    private void Jump(Vector2 dir)
+    {
+
+    }
+}
