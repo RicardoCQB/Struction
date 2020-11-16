aa49a0ab99d7033af644372cf0e873af9483f167
Ricardo Brioso <ricardo_cqb@hotmail.com>
2020-11-16 01:09:42 +0000

    Added parallax script

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    public Transform cam;
+    public float relativeMov = .3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(cam.position.x * relativeMov, transform.position.y);
+    }
+}
