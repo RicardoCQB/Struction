ef943966fee296e8c7df85f954e50481cdd51120
RicardoCQB <ricardo_cqb@hotmail.com>
2020-11-17 11:52:36 +0000

    lasagnaCounter works fully

--- Assets/Scripts/LasagnaCollector.cs
+++ Assets/Scripts/LasagnaCollector.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LasagnaCollector : MonoBehaviour
 {
     private int lasagnaCounter = 0;
 
+    public TextMeshProUGUI textLasagnaCounter;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.transform.tag == "Lasagna")
         {
+            lasagnaCounter++;
+            textLasagnaCounter.text = lasagnaCounter.ToString();
             Destroy(other.gameObject);
         }
     }
