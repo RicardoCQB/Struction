61f8e28022a3f3ba018c22ee1a4503903b250a6b
RicardoCQB <ricardo_cqb@hotmail.com>
2020-11-17 11:37:09 +0000

    lasagna gets destroyed

---
+++ Assets/Scripts/LasagnaCollector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LasagnaCollector : MonoBehaviour
+{
+    private int lasagnaCounter = 0;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.transform.tag == "Lasagna")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
